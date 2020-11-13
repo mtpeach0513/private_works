38dd0b9feed6424ad125ffadb9539a5f700e4c14
momoyama <kanaami1@gmail.com>
2020-11-13 16:47:28 +0900

    add a comment

--- img_download.py
+++ img_download.py
@@ -11,7 +11,7 @@ def download_image(url, timeout=10):
         e = Exception("HTTP status: " + res.status_code)
         raise e
 
-    content_type =  res.headers["content-type"]
+    content_type = res.headers["content-type"]
     if "image" not in content_type:
         e = Exception("Content-Type: " + content_type)
         raise e
@@ -21,13 +21,14 @@ def download_image(url, timeout=10):
 def make_filename(base_dir, number, url):
     ext = os.path.splitext(url)[1]    # 拡張子を取得
     now = datetime.datetime.now()
+    #date_now = "{0:%Y%m%d_%H%M}".format(now)    # python3.5以前用
     date_now = f"{now:%Y%m%d_%H%M}"
-    filename = date_now + ext    # 番号に拡張子をつけてファイル名にする
+    filename = date_now + ext    # 現在時刻 + 拡張子がファイル名
     fullpath = os.path.join(base_dir, filename)
     if os.path.exists(fullpath) == True:
-        filename = date_now + str(number) + ext
+        filename = date_now + str(number) + ext    # 現在時刻 + 番号 + 拡張子がファイル名
         fullpath = os.path.join(base_dir, filename)
-    return filename, fullpath
+    return filename, fullpath    # filenameはmosaic保存時、fullpathはOpenCV画像読み込み時に使う
 
 
 def save_image(filename, image):
@@ -40,22 +41,22 @@ def mosaic_img(base_dir, filename, filepath):
     ratio = 0.1
     small = cv2.resize(src, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
     big = cv2.resize(small, src.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
-    pts = np.array([[500, 720], [1280, 220], [1280, 720]])
+    pts = np.array([[500, 720], [1280, 220], [1280, 720]])    # mosaicかける部分、適宜変更可
     mask = cv2.fillConvexPoly(src, pts, (255, 255, 255))
     img = np.where(mask == 255, big, src)
     fullpath = os.path.join(base_dir, filename)
     cv2.imwrite(fullpath, img)
     #cv2.imshow("mosaic", img)
     #cv2.waitKey()
-    #cv2.destroyWindow()
+    #cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    url_txt = "url_list.txt"
-    images_dir = "picture"
-    mosaic_dir = "mosaics"
+    url_txt = "url_list.txt"    # .jpgなどで終わるURLが入力されたtxtファイル
+    images_dir = "picture"    # pictureディレクトリが必要
+    mosaic_dir = "mosaics"    # mosaicsが必要。これらは作業ディレクトリ内に作成
     idx = 0
-    with open(url_txt, "r") as fin:
+    with open(url_txt, "r") as fin:    # "r"は読み込み専用
         for line in fin:
             url = line.strip()
             filename, filepath = make_filename(images_dir, idx, url)
