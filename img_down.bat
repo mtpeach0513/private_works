2b764bde686fc122a40c6da95c661483eebb14a7
momoyama <kanaami1@gmail.com>
2021-04-23 09:09:55 +0900

    add a comment

--- img_down.bat
+++ img_down.bat
@@ -1,4 +1,6 @@
+@rem img_download.py があるディレクトリを指定
 cd private_works
+@rem 仮想環境を呼び出しているだけ
 call C:\ProgramData\Anaconda3\Scripts\activate.bat
-call activate works
+call activate env_name
 python img_download.py
